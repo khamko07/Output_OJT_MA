bbbc40ef222f3d96b5ccde2e074eea7a8f9c679a
Khamko <khamko@pascaliaasia.com>
2026-05-18 08:44:30 +0700

    first commit

---
+++ src/test/java/Output_OJT_MA/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Output_OJT_MA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
